5a00128e7cfe26e964ec850ea226dc305bc0a0c7
oluwadaprof <adeekotobiloba8@gmail.com>
2022-06-28 16:16:15 +0100

    done with api

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -8,14 +8,63 @@ router.post("/", verify, async(req, res) => {
     if (req.user.isAdmin) {
         const newList = new List(req.body)
         try {
-            const savedMovie = await newMovie.save();
-            res.status(201).json(savedMovie)
+            const savedList = await newList.save();
+            res.status(201).json(savedList)
         } catch (error) {
             res.status(500).json(error)
         }
     } else {
-        res.status(403).json("you are not allowed to update account")
+        res.status(403).json("you are not allowed to update list")
     }
+})
+
+//delete list
+router.delete("/:id", verify, async(req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            await List.findByIdAndDelete(req.params.id)
+            res.status(201).json("list deleted")
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    } else {
+        res.status(403).json("you are not allowed to update list")
+    }
+})
+
+//get list 
+router.get("/", verify, async(req, res) => {
+    const typeQuery = req.query.type;
+    const genreQuery = req.query.genre;
+    let list = []
+
+    try {
+        if (typeQuery) {
+            if (genreQuery) {
+                list = await List.aggregate([{
+                        $sample: {
+                            size: 10
+                        }
+                    },
+                    { $match: { type: typeQuery, genre: genreQuery } }
+                ])
+            } else {
+                list = await List.aggregate([{
+                        $sample: {
+                            size: 10
+                        }
+                    },
+                    { $match: { type: typeQuery } }
+                ])
+            }
+        } else {
+            list = await List.aggregate([{ $sample: { size: 10 } }])
+        }
+        res.status(201).json(list)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+
 })
 
 
